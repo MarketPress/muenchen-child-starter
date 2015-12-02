21a9f63de7c80b86ad8b711a7de46281e157bc1d
tinabillinger <tina@billinger.com>
2015-12-02 16:16:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Muenchen Child Starter
+
+A child theme starter kit for WordPress.
+
+## Description
+This is a starter kit for a custom child theme. Child themes in WordPress enable you to override theme templates and stylesheets with your own customizations in a safe way. No need to touch an original theme file!
+
+This starter kit uses our own theme [Muenchen](http://marketpress.com/product/muenchen/) as the parent theme. Feel free to use it for your own projects, or hack it and create your own child theme starter kit.
+
+Also, as in Muenchen and many other themes, minified stylesheets can be loaded by default. In order to load minified versions of stylesheets and scripts, visit functions.php and set a filter as documented inline.
+
+## Ressources
+
+* [The WordPress Codex on Child Themes](http://codex.wordpress.org/Child_Themes)
+* [Muenchen Theme Documentation](http://marketpress.de/doku/theme-muenchen/)
+* [Child-Theme erstellen und anpassen (DE)](http://make.marketpress.com/themes/child-themes-erstellen-anpassen)
+* [Tutorial on Child Themes (EN)](http://marketpress.com/2013/creating-customizing-child-themes)
+
+## Contributors
+
+* [@inpsyde](https://github.com/inpsyde)
+* [@dasllama](https://github.com/dasllama)
+* [@Deku86](https://github.com/Deku86)
+
+**Brought to you by**
+
+[![MarketPress.com](/assets/img/mp-logo.png)](http://marketpress.com)
+
+## Changelog
+
+### 1.0
+
+* Initial release.
---
+++ functions.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Functions and definitions for Muenchen Child.
+ *
+ * @package    WordPress
+ * @subpackage Muenchen_Child
+ * @version    1.0
+ * @author     marketpress.com
+ */
+
+add_action( 'after_setup_theme', 'munich_child_setup' );
+/**
+ * Sets up theme defaults and registers support for various WordPress features
+ * of Muenchen Child Theme.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support for post thumbnails.
+ *
+ * @since   05/07/2015
+ * @return  void
+ */
+function munich_child_setup() {
+
+	/* The .min suffix for stylesheets and scripts.
+	 *
+	 * In order to provide a quick start, this child theme by default will load
+	 * regular CSS and javascript files (whereas its parent theme loads
+	 * minified versions of its stylesheets and scripts by default).
+	 *
+	 * If you want your child theme to default on minified stylesheets and scripts,
+	 * set the following filter:
+	 *
+	 * if( function_exists( 'munich_get_script_suffix' ) ) {
+	 *     add_filter( 'munich_child_starter_get_script_suffix', 'munich_get_script_suffix' );
+	 * }
+	 *
+	 * Don’t forget to actually add applicable .min files to your child theme first!
+	 *
+	 * You can then temporarily switch back to unminified versions of the same
+	 * files by setting the constant SCRIPT_DEBUG to TRUE in your wp-config.php:
+	 * define( 'SCRIPT_DEBUG', TRUE );
+	 */
+
+	// Loads the child theme's translated strings
+	load_child_theme_textdomain(
+		'muenchen-child-starter',
+		get_stylesheet_directory() . '/languages'
+	);
+
+	if( !is_admin() ){
+
+		// child theme styles
+		add_filter( 'munich_get_styles', 'munich_child_filter_munich_get_styles_add_stylesheets' );
+
+	}
+}
+
+/**
+ * Adding our own styles for our child theme
+ *
+ * @wp-hook munich_get_styles
+ * @param   Array $styles
+ * @return  Array $styles
+ */
+function munich_child_filter_munich_get_styles_add_stylesheets( array $styles = array() ) {
+
+	// add suffix
+	$suffix = apply_filters( 'munich_child_starter_get_script_suffix', '' );
+
+	// getting the theme-data
+	$theme_data = wp_get_theme();
+
+	// adding our own styles to
+	$styles[ 'munich_child' ] = array(
+		'src'       => get_stylesheet_directory_uri() . '/style' . $suffix . '.css',
+		'deps'      => NULL,
+		'version'   => $theme_data->Version,
+		'media'     => NULL
+	);
+
+	return $styles;
+
+}
